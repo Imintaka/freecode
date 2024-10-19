0213903e0359d1cebf5b24f3cb68f44210cfade2
Imintaka <demin1556986@mail.ru>
2024-10-19 15:00:11 +0300

    Mutate an Array Declared with const

---
+++ ES6/2.js
@@ -0,0 +1,11 @@
+//Mutate an Array Declared with const
+
+const s = [5, 7, 2];
+function editInPlace() {
+    // Only change code below this line
+    s[0] -= 3
+    s[1] -= 2
+    s[2] += 5
+    // Only change code above this line
+}
+editInPlace();
